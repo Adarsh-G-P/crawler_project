143ce98fab6fe00c8f82b3934bc273333c434f41
Adarsh-G-P <adarshgp96@gmail.com>
2023-06-27 01:31:34 +0530

    added a fullscreen background image

--- fe/src/index.js
+++ fe/src/index.js
@@ -1,3 +1,10 @@
+
+// sets up the React application, renders the main <App> component, 
+//and provides a hook for measuring and reporting web vitals. 
+//The rendered application will be injected into the specified root element in the HTML document,
+// replacing the placeholder <div id="root"></div>.
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -8,7 +15,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+//Creates a root element for rendering the React application.
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//Renders the React application inside the root element.
 root.render(
   <React.StrictMode>
     <App />
